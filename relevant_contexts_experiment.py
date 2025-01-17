e198a784c3efa960989a4daada3b662053d62877
Oana Madalina Fron <o.m.fron-1@student.tudelft.nl>
2025-01-17 16:09:56 +0100

    Updated results 4o-mini

--- relevant_contexts_experiment.py
+++ relevant_contexts_experiment.py
@@ -3,6 +3,7 @@ from experiment import Experiment, ExperimentType
 if __name__ == "__main__":
     experiment = Experiment(experiment_type=ExperimentType.RELEVANT_CONTEXTS, TOP_K=5)
     experiment.run_experiment(test_mode=True)
+    # experiment.run_metrics_on_saved_results("results/oracle_contexts_top_k_10-4o-mini/final_questions.csv")
                 
 
         
