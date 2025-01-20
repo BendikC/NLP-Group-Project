304702d59a50187cfa2618b1851b4422f60709fd
Todor-cmd <79723091+Todor-cmd@users.noreply.github.com>
2025-01-20 21:16:11 +0100

    all adore experiments complete

---
+++ pipeline_components/adore_contriever.py
@@ -0,0 +1,74 @@
+from transformers import AutoModel
+from dexter.retriever.dense.HfRetriever import HfRetriever
+from typing import Dict, List
+from corpus_management.load_corpus import load_encoded_corpus
+from dexter.data.datastructures.question import Question
+from dexter.data.datastructures.evidence import Evidence
+from dexter.utils.metrics.SimilarityMatch import SimilarityMetric
+from corpus_management.encode_corpus import encode_corpus
+import torch
+
+class AdoreContriever(HfRetriever):
+    def __init__(self, config, model_path: str):
+        super().__init__(config)
+        self.question_encoder = AutoModel.from_pretrained(model_path)
+        if torch.cuda.is_available():
+            self.question_encoder.cuda()
+    
+    
+    def load_index_if_available(self):
+        """
+        Override of HfRetriever's load_index_if_available to load pre-encoded corpus from memmap.
+        This method will be called by retrieve() since we inherit from HfRetriever.
+        
+        Returns:
+            Tuple[corpus_embeddings, index_present]
+        """
+        return load_encoded_corpus()
+    
+    
+    def retrieve(self, 
+               corpus: List[Evidence], 
+               queries: List[Question], 
+               top_k: int, 
+               score_function: SimilarityMetric,
+               return_sorted: bool = True, 
+               chunk: bool = False,
+               chunksize = None,
+               **kwargs) -> Dict[str, Dict[str, float]]:
+
+            
+        self.logger.info("Encoding Queries...")
+
+        query_embeddings = self.encode_queries(queries, batch_size=self.batch_size)
+          
+        if chunk:
+            results = self.retrieve_in_chunks(corpus, 
+                                              queries,top_k=top_k,
+                                              score_function=score_function,return_sorted=return_sorted,
+                                              chunksize=chunksize)
+            return results
+   
+        self.logger.info("Encoding Corpus in batches... Warning: This might take a while!")
+        #self.logger.info("Scoring Function: {} ({})".format(self.score_function_desc[score_function], score_function))
+        embeddings, index_present = self.load_index_if_available()
+
+        if index_present:
+            corpus_embeddings = embeddings
+        else:
+            self.logger.info("Index not found, encoding corpus...")
+            corpus_embeddings = encode_corpus("data/embeddings", self.config)
+            
+
+        # Compute similarites using either cosine-similarity or dot product
+        cos_scores = score_function.evaluate(query_embeddings,corpus_embeddings)
+        # Get top-k values
+        cos_scores_top_k_values, cos_scores_top_k_idx = torch.topk(cos_scores, min(top_k+1, len(cos_scores[1])), dim=1, largest=True, sorted=return_sorted)
+        cos_scores_top_k_values = cos_scores_top_k_values.cpu().tolist()
+        cos_scores_top_k_idx = cos_scores_top_k_idx.cpu().tolist()
+        response = {}
+        for idx, q in enumerate(queries):
+            response[q.id()] = {}
+            for index, id in enumerate(cos_scores_top_k_idx[idx]):
+                response[q.id()][corpus[id].id()] = float(cos_scores_top_k_values[idx][index])
+        return response
--- relevant_contexts_experiment.py
+++ relevant_contexts_experiment.py
@@ -1,8 +1,8 @@
 from experiment import Experiment, ExperimentType
 
 if __name__ == "__main__":
-    experiment = Experiment(experiment_type=ExperimentType.RELEVANT_CONTEXTS, TOP_K=5)
-    experiment.run_experiment(test_mode=True)
+    experiment = Experiment(experiment_type=ExperimentType.ADORE_CONTEXTS, TOP_K=5)
+    experiment.run_experiment(test_mode=False)
     # experiment.run_metrics_on_saved_results("results/oracle_contexts_top_k_10-4o-mini/final_questions.csv")
                 
 
